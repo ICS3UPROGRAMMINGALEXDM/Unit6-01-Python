7d5a70670ac226e14315a1befe58927a75599e56
Ubuntu <ubuntu@ip-172-31-17-170.ec2.internal>
2022-05-19 13:40:50 +0000

    Done

---
+++ average.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Created By: Alex De Meo
+# Date: 03/25/2022
+# Description: Generates 10 random numbers and calculates the average
+import constants
+import random
+
+
+def main():
+    num_arr = []
+    for counter in range(constants.MAX_SIZE):
+        rand_num = random.randint(constants.MIN_NUM, constants.MAX_NUM)
+        num_arr.append(rand_num)
+        print("Position {} is {}".format(counter, num_arr[counter]))
+    sum_num = 0
+    for counter in range(constants.MAX_SIZE):
+        sum_num += num_arr[counter]
+    avg = sum_num / constants.MAX_SIZE
+
+    print("\nThe average is {}".format(avg))
+
+
+if __name__ == "__main__":
+    main()
---
+++ constants.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# Created By: Alex De Meo
+# Date: 03/25/2022
+# Description: constants file for average program
+
+MAX_NUM = 100
+MAX_SIZE = 10
+MIN_NUM = 0
